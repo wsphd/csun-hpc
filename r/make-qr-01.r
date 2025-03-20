9da609615fd7669d060bd09ebe0389c4a8406aaf
Wayne Smith <wayne.smith@csun.edu>
2025-03-19 19:11:55 -0700

    update

--- r/make-qr-01.r
+++ r/make-qr-01.r
@@ -4,17 +4,17 @@
 
 # wayne.smith@csun.edu
 # created: Jan 29, 2021
-# updated: Mar 14, 2025
+# updated: Mar 19, 2025
 
 
 # we need the 'qrcode' package (to provide the 'qr_code' function that we need) -- '> install.packages( "qrcode" )'
 require( "qrcode" )
 
 # generate the code
-qr_code.code <- qr_code( "https://github.com/wsphd/csun-hpr" )
+qr_code.code <- qr_code( "https://github.com/wsphd/csun-hpc" )
 
 # print the code on the screen
 print( qr_code.code )
 
 # save the code as a .svg file
-generate_svg( qr_code.code, filename = "c:/wsmith/github/csun-hpr/images/qr.svg" )
+generate_svg( qr_code.code, filename = "c:/wsmith/github/csun-hpc/images/qr.svg" )
